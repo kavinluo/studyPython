ebccd83aaa375a5b919d377e13080f0158b228d1
kavin <luoxixi_com@outlook.com>
2019-12-08 04:40:56 +0000

    try except

---
+++ err.py
@@ -0,0 +1,20 @@
+
+# try :
+#     print(6+6)
+# except :
+#     print("this is err")
+
+# try:
+#     print('66  \nyyy'+999)
+# except Exception:
+#     print("hi,this coding is err")
+
+# try:
+#     print("55" +7)
+# except ArithmeticError as e:
+#     print(e)
+
+try:
+    print(6+'88')
+except ArithmeticError as E:
+    print(E)
